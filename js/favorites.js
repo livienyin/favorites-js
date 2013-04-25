f0f1f5bf308985541b2963672ee8e071d396fa03
Livien Yin <LivienYin@gmail.com>
2013-04-25 11:05:58 -0700

    removed redundent console.log for favoritesList(favorites)

--- js/favorites.js
+++ js/favorites.js
@@ -2,8 +2,8 @@ favorites = ["mangoes", "pineapples", "dragonfruit", "guava"];
 
 function favoritesList(array) {
     for(var i = 0; i < array.length; i += 1) {
-        console.log("My #" + (i + 1) + " favorite fruit is " + array[i])
+        console.log("My #" + (i + 1) + " favorite fruit is " + array[i]);
     };
 }
 
-console.log(favoritesList(favorites));
+favoritesList(favorites);
